c8c8114986911c0bced6c4042181c33a6fc5169c
Mohan K Nalluri <mohankn603@gmail.com>
2026-03-07 23:47:07 -0500

    fix CORS config variable name

--- src/main/java/finance/com/MyFinance/com/config/SecurityConfig.java
+++ src/main/java/finance/com/MyFinance/com/config/SecurityConfig.java
@@ -40,7 +40,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5174",
+                "https://myfinance-frontend.vercel.app/"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
